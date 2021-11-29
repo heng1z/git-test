f04dddd3d12ab809a1d1db21647c8faf818bade6
zhangheng <zhangheng@qq.com>
2021-11-29 13:21:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zh
+ * @create 2021-11-29 13:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
