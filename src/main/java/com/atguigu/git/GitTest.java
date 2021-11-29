8f4b05032e497700e8462579b6adb2e3025ba697
zhangheng <zhangheng@qq.com>
2021-11-29 13:33:46 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2222");
         System.out.println("hello git333");
+        System.out.println("hello git444");
     }
 }
